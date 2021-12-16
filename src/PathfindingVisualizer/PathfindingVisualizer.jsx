4b7435effdb48af857ddfa3fa855fe2eb434d46f
Diogo Santos <diogodeese@gmail.com>
2021-12-16 01:49:09 +0000

    Created a Bar

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,51 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Node from "./Node/Node";
-import { dijkstra, getNodesInShortestPathOrder } from "../Algorithms/Dijkstra";
+import { visualizeDijkstra } from "../Algorithms/Dijkstra";
+import { getInitialGrid } from "./InitalGrid";
 import "./PathfindingVisualizer.css";
 
-  // Setting up the default values
-  const START_NODE_ROW = 10;
-  const START_NODE_COL = 15;
-  const FINISH_NODE_ROW = 10;
-  const FINISH_NODE_COL = 35;
+// Setting up the default values
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
 
+const algorithms = ["Dijkstra"];
 
 export default function PathfindingVisualizer() {
+  const [choosenAlgorithm, setChoosenAlgorithm] = useState(algorithms[0]);
   const [grid, setGrid] = useState([]);
   const [mouseIsPressed, setMouseIsPressed] = useState(false);
 
-  // Creates single nodes
-  const createNode = (col, row) => {
-    return {
-      col,
-      row,
-      type:
-        row === START_NODE_ROW && col === START_NODE_COL
-          ? "start"
-          : row === FINISH_NODE_ROW && col === FINISH_NODE_COL
-          ? "finish"
-          : "",
-      distance: Infinity,
-      isVisited: false,
-      previousNode: null,
-    };
-  };
-
   useEffect(() => {
     // Creates the whole grid with the nodes
-    const getInitialGrid = () => {
-      const grid = [];
-      for (let row = 0; row < 20; row++) {
-        const currentRow = [];
-        for (let col = 0; col < 50; col++) {
-          currentRow.push(createNode(col, row));
-        }
-        grid.push(currentRow);
-      }
-      return grid;
-    };
-
-    const grid = getInitialGrid();
+    const grid = getInitialGrid(
+      START_NODE_ROW,
+      START_NODE_COL,
+      FINISH_NODE_ROW,
+      FINISH_NODE_COL
+    );
     setGrid(grid);
   }, []);
 
@@ -66,64 +45,27 @@ export default function PathfindingVisualizer() {
     setMouseIsPressed(false);
   }
 
-  // Animates the Dijktra algorithm working
-  function animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
-    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
-      if (i === visitedNodesInOrder.length) {
-        setTimeout(() => {
-          animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
-        return;
-      }
-      setTimeout(() => {
-        const node = visitedNodesInOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          "node node-visited";
-      }, 10 * i);
-    }
-  }
-
-  // Animates the shortest path when the algorithm reaches the end point
-  function animateShortestPath(nodesInShortestPathOrder) {
-    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
-      setTimeout(() => {
-        const node = nodesInShortestPathOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          "node node-shortest-path";
-      }, 50 * i);
-    }
-  }
-
-  // Calls all the needed functions to get the algorithm up and running
-  function visualizeDijkstra(grid) {
-    const startNode = grid[START_NODE_ROW][START_NODE_COL];
-    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
-    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
-    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
-  }
-
   // Changes the type of the node to "Wall" when a node is pressed or passed over
   const getNewGridWithWallToggled = (grid, row, col) => {
     const newGrid = grid.slice();
     const node = newGrid[row][col];
-    let newNode
+    let newNode;
 
-    if(node.type === "start" || node.type === "finish") {
+    if (node.type === "start" || node.type === "finish") {
       newNode = {
         ...node,
       };
-    } else if(node.type === "wall") {
+    } else if (node.type === "wall") {
       newNode = {
         ...node,
-        type: ""
+        type: "",
       };
     } else {
       newNode = {
         ...node,
-        type: "wall"
+        type: "wall",
       };
-    };
+    }
 
     newGrid[row][col] = newNode;
     return newGrid;
@@ -131,12 +73,47 @@ export default function PathfindingVisualizer() {
 
   return (
     <div className="pathfinding-visualizer">
-      <button onClick={() => visualizeDijkstra(grid)} className="button">
-        Visualize Dijkstra's Algorithm
-      </button>
-      <button onClick={() => window.location.reload()} className="button">
-        Clear Border
-      </button>
+      <div className="bar">
+        <h1 className="title">Pathfinding Algorithms</h1>
+
+        <select
+          onChange={(e) => {
+            setChoosenAlgorithm(e.target.value);
+          }}
+        >
+          {algorithms.map((algorithm) => {
+            return <option>{algorithm}</option>;
+          })}
+        </select>
+        <button
+          onClick={() =>
+            visualizeDijkstra(
+              grid,
+              START_NODE_ROW,
+              START_NODE_COL,
+              FINISH_NODE_ROW,
+              FINISH_NODE_COL
+            )
+          }
+          className="button"
+        >
+          Visualize {choosenAlgorithm}
+        </button>
+        <button
+          onClick={() => {
+            const restartGrid = getInitialGrid(
+              START_NODE_ROW,
+              START_NODE_COL,
+              FINISH_NODE_ROW,
+              FINISH_NODE_COL
+            );
+            setGrid(restartGrid);
+          }}
+          className="button"
+        >
+          Clear Border
+        </button>
+      </div>
       <div className="grid">
         {grid.map((row, rowIndex) => {
           return (
